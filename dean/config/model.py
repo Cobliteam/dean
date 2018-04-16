516f7759db4d2779db7bc256dbbd1e6192aa730c
Daniel Miranda <danielkza2@gmail.com>
2018-04-16 15:17:55 -0300

    config: generate absolute clone URL for file scheme

--- dean/config/model.py
+++ dean/config/model.py
@@ -2,6 +2,7 @@ import os
 import re
 from enum import Enum
 from typing import Any, Collection, Dict, Optional
+from urllib.parse import urlsplit, urlunsplit
 
 from dataclasses import dataclass
 
@@ -25,7 +26,23 @@ class Repository:
     revision: str = None
     type: RepositoryType = RepositoryType.git
 
+    def clone_url(self):
+        if re.match(r'^.+?@.+?:.+?$', self.url):  # SSH
+            return self.url
 
+        url_fields = urlsplit(self.url)
+        if url_fields.scheme not in ('', 'file'):
+            return self.url
+
+        path = url_fields.path
+        if path.startswith('/'):
+            return self.url
+
+        new_fields = list(url_fields)
+        new_fields[0] = 'file'
+        new_fields[2] = os.path.abspath(path)
+
+        return urlunsplit(new_fields)
 
 
 @dataclass
